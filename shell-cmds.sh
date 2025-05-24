68a637445ed7417e5bbe355ea6ed7dddefdb2e7b
ayeshawaheed7 <ayeshawaheed451@gmail.com>
2025-05-24 09:50:22 +0500

    added docker creds on ec2 server

--- shell-cmds.sh
+++ shell-cmds.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env groovy
 
 export IMAGE=$1
+source ~/.docker-creds.env
+
+echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} -password-stdin
 docker-compose -f docker-compose.yaml up --detach
 echo 'successfully deployed!'

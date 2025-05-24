ddf0fa8ac599317a4affab92a01bd10ece2ffb55
ayeshawaheed7 <ayeshawaheed451@gmail.com>
2025-05-24 09:56:17 +0500

    fix typo

--- shell-cmds.sh
+++ shell-cmds.sh
@@ -3,6 +3,6 @@
 export IMAGE=$1
 source ~/.docker-creds.env
 
-echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} -password-stdin
+echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
 docker-compose -f docker-compose.yaml up --detach
 echo 'successfully deployed!'
